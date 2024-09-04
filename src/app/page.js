0d0ace4d986f6dc1ceab8b7d3b2ffa6b3a365941
Shaurya Jha <shauryajha35@gmail.com>
2024-09-04 13:13:02 +0530

    Add config files

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      main component
+    </main>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-    return (
-      <main>
-        hi there
-      </main>
-    );
-  }
